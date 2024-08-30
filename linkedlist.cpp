0ca08ba3f01b7655d1123957067f8e2e3b469f1c
kemeer <83573341+kemeer@users.noreply.github.com>
2024-08-30 22:38:10 +0300

    linkedlist.cpp

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -25,16 +25,27 @@ void ekle(node *r,int x){
 
 int main(){
 	
+	//Olusturma
 	node *root;
 	root = (node*)malloc(sizeof(node));
 	root->next = NULL;
 	root->x = 500;
 	node *iter = root;//iter gezici 
 	
-	for(int i=0;i<3;i++){
+	for(int i=0;i<5;i++){
 		ekle(root,i*10);
 	}
 	
+	bastir(root);
+	
+	//Araya ekleme
+	for(int i=0;i<3;i++)iter = iter->next;
+	node* temp = (node*)malloc(sizeof(node));
+	temp->next = iter->next;
+	iter->next = temp;
+	temp->x = 100;
+	
+	printf("\n\n");
 	bastir(root);
 	
 	return 0;
